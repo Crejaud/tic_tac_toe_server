e87181bca9fb63a44f739abcc169fea3669df5d1
harryli0088 <harryli0088@gmail.com>
2017-04-26 14:45:43 -0400

    semicolon

--- ttt_logic.h
+++ ttt_logic.h
@@ -75,7 +75,7 @@ int open_listenfd(int portno);
 int Open_listenfd(int portno);
 
 int make_move(int player, int x, int y);
-int check_winner(int x, int y)
+int check_winner(int x, int y);
 
 void process_p1(int fd, struct sockaddr_in clientaddr, socklen_t clientlen);
 void process_p2(int fd, struct sockaddr_in clientaddr, socklen_t clientlen);
